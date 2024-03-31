00079e57dd41948e5dcb1ee2121bb0fb5dc92c0e
Serpent1776 <j.d.hemling@email.msmary.edu>
2024-03-31 14:31:46 -0400

    homework 9

---
+++ Py stuff/hw9_problem.py
@@ -0,0 +1,28 @@
+from threading import *
+def f():
+    lock.acquire()
+    for i in range(1000000000000, 2000000000000):
+        for d in range(2, int(i**.5)+2):    
+            if i % d == 0:
+                break
+        else:    
+            print(i)
+    lock.release()       
+
+def g():
+    lock.acquire()
+    for i in range(2000000000000, 3000000000000):
+        for d in range(2, int(i**.5)+2):    
+            if i % d == 0:
+                break
+        else:    
+            print(i)
+    lock.release()                 
+            
+
+lock = Lock()
+t1 = Thread(target = f)
+t2 = Thread(target = g)
+
+t1.start()
+t2.start()
